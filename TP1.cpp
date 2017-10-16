2cadfaeafe1fa9e1bd17fb76dd7c917565234dbb
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-10-16 12:44:46 +0200

    Commentaires ajoutés

--- TP1.cpp
+++ TP1.cpp
@@ -4,22 +4,35 @@
 #include <stdlib.h>
 using namespace std;
 
+// Variable aléatoire uniforme entre 0 et 1
 float ranf()
 {
-  return (float) rand()/RAND_MAX; //fdsqfaze
+  return (float) rand()/RAND_MAX;
 }
 
-float source() { return ranf(); }
-
-float f_gene(float mu, float sigma)
-{ 
-  return -mu * log(ranf()) / sigma + source();
-}
-
-vector<float> trajs(float mu, float sigma, int n)
+/* Terme source, suivant qu'il est uniforme (typ_source vrai)
+                  ou non-nul seulement en 0 (typ_source faux) */
+float source(bool typ_source)
 {
-  vector<float> part(n); // n est la taille du tableau part
-  //int i;
+  if (typ_source)
+    return ranf();
+  else
+    return 0;
+}
+
+/* Variable aléatoire suivant la densité de probabilité
+
+du libre parcours d'un neutron */
+float f_gene(float mu, float sigma, bool typ_source)
+{ 
+
+  return -mu * log(ranf()) / sigma + source(typ_source);
+}
+
+// Echantillon de taille n de cette variable aléatoire
+vector<float> trajs(float mu, float sigma, int n, bool typ_source)
+{
+  vector<float> part(n);
   for(int i = 0 ; i < n ; i++)
   {
     part.at(i) = f_gene(mu,sigma);
@@ -27,13 +40,15 @@ vector<float> trajs(float mu, float sigma, int n)
   return part;
 }
 
-vector<int> denom(const vector<float>& part, int nb_seg)
+/* Flux de particules dénombrant cet échantillon
+   sur chaque segment de l'intervalle total */
+vector<int> denom(const vector<float>& part, int nb_seg, bool typ_source)
 {
-  vector<int> flux(nb_seg + 2,0); //initialise toutes les valeurs à 0
-  for (auto const & val : part) // sans indice
+  vector<int> flux(nb_seg + 2,0);
+  for (auto const & val : part)
   {
-    if (val>=1) flux.at(nb_seg+1)++; // ou flux[nb_seg] mais pas de vérif ...
-    else if (val< 0) flux.at(0)++;
+    if (val>=1) flux.at(nb_seg+1)++; // Les deux cas où le particule ...
+    else if (val< 0) flux.at(0)++;   // ... sort de l'intervalle
     else flux.at( floor(nb_seg*val) )++;
   }
   return flux;
@@ -46,16 +61,18 @@ int main(int argc, char**argv)
         sigma;
   int nb_part,
       nb_seg;
+  bool typ_source;
   mu = atof(argv[1]);
   sigma = atof(argv[2]);
-  nb_part = atoi(argv[3]); // i pour pas float bro
-  nb_seg  = atoi(argv[4]);
+  nb_part = atoi(argv[3]);    // Nombre de particules de l'échantillon
+  nb_seg  = atoi(argv[4]);    // Finesse de segmentation de l'intervalle pour calculer le flux
+  typ_source = atoi(argv[5]); // Source uniforme ou seulement entrante à gauche
   
-  vector<float> part = trajs(mu, sigma, nb_part);
-  //for(auto const & val : part) // const -> pas modif de val, promis
-  //  cout << val << endl;  // pas obligé les accolades pour boucles avec une ligne d'exe
-  vector<int> flux = denom(part, nb_seg);
-  // for(int i = 1 ; i < nb_seg ; i++) // const -> pas modif de val, promis
-  //   cout << (float) i / nb_seg << " " << (float) flux.at(i) / nb_part << endl; // " -> ouvre des strings ma gueuuule EEEEEEEEET (float) avant division euclidienne
+  vector<float> part = trajs(mu, sigma, nb_part,typ_source);
+  //for(auto const & val : part)
+  //  cout << val << endl;
+  vector<int> flux = denom(part, nb_seg,typ_source);
+  // for(int i = 1 ; i < nb_seg ; i++)
+  //   cout << (float) i / nb_seg << " " << (float) flux.at(i) / nb_part << endl;
   return EXIT_SUCCESS;
 }
